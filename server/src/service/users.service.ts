fb51f24c7dd33e56d6588e65b896bddf39016b87
NV-Phong <0h9ng03@gmail.com>
2024-09-29 23:28:40 +0700

    [chore] format code

--- server/src/service/users.service.ts
+++ server/src/service/users.service.ts
@@ -1,47 +1,46 @@
-  // users.service.ts
-  import { Injectable } from '@nestjs/common';
-  import { InjectModel } from '@nestjs/mongoose';
-  import { Model } from 'mongoose';
-  import { User, UserDocument } from '../schema/user.schema';
-  import { CreateUserDto } from 'src/dto/create-user.dto';
+// users.service.ts
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from '../schema/user.schema';
+import { CreateUserDto } from 'src/dto/create-user.dto';
 
-  @Injectable()
-  export class UsersService {
-    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-    async create(createUserDto: CreateUserDto): Promise<User> {
-      const createdUser = new this.userModel(createUserDto);
-      return createdUser.save();
-    }
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
+  }
 
-    async findAll(): Promise<User[]> {
-      return this.userModel.find().exec();
-    }
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
 
-    async findByName(name: string): Promise<User[]> {
-      return this.userModel.find({ name: new RegExp(name, 'i') }).exec();
-    }
+  async findByName(name: string): Promise<User[]> {
+    return this.userModel.find({ name: new RegExp(name, 'i') }).exec();
+  }
 
-    async findOne(id: string): Promise<User> {
-      console.log('Hey I found this guy: ', id);
-      return await this.userModel.findOne({ _id: id }).exec();
-    }
-    async findbyemail(email: string): Promise<User> {
-      console.log('Hey I found this guy: ', email);
-      return await this.userModel.findOne({ email: email }).exec();
-    }
+  async findOne(id: string): Promise<User> {
+    console.log('Hey I found this guy: ', id);
+    return await this.userModel.findOne({ _id: id }).exec();
+  }
+  async findbyemail(email: string): Promise<User> {
+    console.log('Hey I found this guy: ', email);
+    return await this.userModel.findOne({ email: email }).exec();
+  }
 
-    async findbyID(id: string): Promise<User> {
-      console.log('vãi l : ', id);
-      return this.userModel.findById(id);
-    }
-    
-    async deleteUser(id: string) {
-     
-      try {
-        const deletedUser = await this.userModel.findByIdAndDelete({_id : id }).exec();
-      } catch (error) {
-        console.log('méo tìm thây người dùng ', id);
-      }
+  async findbyID(id: string): Promise<User> {
+    console.log('vãi l : ', id);
+    return this.userModel.findById(id);
+  }
+
+  async deleteUser(id: string) {
+    try {
+      await this.userModel.findByIdAndDelete({ _id: id }).exec();
+    } catch (error) {
+      console.log('méo tìm thây người dùng ', id, error);
     }
   }
+}
